1847fdce04fbd6da552d1a15cfe1dcffb237a30b
Jigme Tenzin Chogyel <jigmetenzinchogyel@gmail.com>
2023-10-01 16:35:55 +0600

    log

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -1,3 +1,4 @@
+
 function SignIn() {
   return (
     <div>SignIn</div>
@@ -5,3 +6,25 @@ function SignIn() {
 }
 
 export default SignIn
+// import SigninForm from "./SigninForm"
+// import logo from '../assets/logo.png'
+
+// function SignIn() {
+//   return (
+//     <div className="flex font-Plus-Jakarta-Sans">
+//       <div className="w-1/3 h-screen flex flex-col gap-6">
+//         <div className="flex flex-col items-center gap-1 pt-10">
+//           <img src={ logo } className=""/>
+//           <h1 className="text-2xl font-bold ">Welcome Back!</h1>
+//           <p className="text-xs text-slate-400">Greetings! Weclome to member login</p>
+//         </div>
+//         <div>
+//           <SigninForm />
+//         </div>
+//       </div>
+//       <div className="w-2/3 bg-slate-100 h-c">2</div>
+//     </div>
+//   )
+// }
+
+// export default SignIn
