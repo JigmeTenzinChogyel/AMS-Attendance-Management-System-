e297647643952ebd9aebe5c0810b5e1570851e17
Jigme Tenzin Chogyel <jigmetenzinchogyel@gmail.com>
2023-10-03 20:37:10 +0600

    create event

--- src/layouts/EventLayout.jsx
+++ src/layouts/EventLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 function EventLayout() {
 
   return (
-    <div className="m-5 bg-white">
+    <div className="m-3 py-12 bg-white font-Roboto flex justify-center items-center">
         <Outlet/>
     </div>
   )
